8fc89c42e701340d33e89046c33f7625df9e8e36
Angeline <angeline07561@gmail.com>
2016-04-02 17:32:36 +1100

    Handle input with correction

--- GameTest.java
+++ GameTest.java
@@ -4,27 +4,29 @@ public class GameTest {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[][] 2DArray;
-        int entries; // store board state entries per row and col
+        int[][] boardState;
+        int entries; // store max board state entries per row or col
         try {
             int boardSize = input.nextInt();
             entries = 4*boardSize - 1;
-            2DArray = new int[entries][entries];
-            int row, col = 0;
-            while (input.hasNextInt()) {
-                // store board state into a 2D array
-                int[row][col] = input.nextInt();
-                if (col < entries) {
-                    col++;
-                }
-                else if (col == entries) {
-                    if (row >= entries) {
-                        System.out.println("Exceeded max input for board state");
+            boardState = new int[entries][entries];
+            int row= 0, col = 0;
+            while (input.hasNext()) {
+                String tempStr = input.nextLine();
+                Scanner innerInput = new Scanner(tempStr);
+                while (innerInput.hasNextInt()) {
+                    boardState[row][col] = innerInput.nextInt();
+                    if (col > entries) {
+                        System.out.println("Exceeded max col input for board state");
                         System.exit(0);
                     }
-                    row++;
-                    col = 0;
+                    col++;
                 }
+                if (row > entries) {
+                    System.out.println("Exceeded max row input for board state");
+                    System.exit(0);
+                }
+                row++;
             }
         }
         catch (Exception e) {
